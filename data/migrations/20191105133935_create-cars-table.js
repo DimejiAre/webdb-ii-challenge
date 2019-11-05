b90469150fa999337ed9589dd299f408a453a6ef
DimejiAre <olardi2@gmail.com>
2019-11-05 14:18:58 +0100

    create db and cars table using migrations

---
+++ data/migrations/20191105133935_create-cars-table.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('cars', table => {
+      table.increments('id')
+      table.text('VIN').notNullable();
+      table.text('make').notNullable();
+      table.text('model').notNullable();
+      table.integer('mileage').notNullable();
+      table.text('transmission_type')
+      table.text('status')
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('cars');
+};
